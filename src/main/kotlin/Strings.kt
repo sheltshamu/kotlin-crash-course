3270cb3d65370ee652e764d01197dea8c864c401
sheltons <shedzafyah2@gmail.com>
2023-06-27 21:38:35 +0200

    exploring String DataType

---
+++ src/main/kotlin/Strings.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val name = "Shelton"
+    println(name)
+    println(name.length)
+    println(name.toUpperCase())
+    println(name.toLowerCase())
+    println(name.isEmpty())
+    println(name.isNotEmpty())
+}
