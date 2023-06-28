cc00d0cc3bb18464e94267d9e000f4de5817c505
sheltons <shedzafyah2@gmail.com>
2023-06-28 11:46:13 +0200

    the Maths class

---
+++ src/main/kotlin/TheMath.kt
@@ -0,0 +1,11 @@
+import kotlin.math.PI
+import kotlin.math.max
+import kotlin.math.min
+
+fun main (){
+    var numberOne = 10
+    var numberTwo = 5
+
+    println(min(numberOne,numberTwo))
+    println(PI)
+}
