64dc71bd7d2b98486a310872ae3994fbda0f6465
sheltons <shedzafyah2@gmail.com>
2023-06-28 12:04:16 +0200

    Incrementation and Decremental Operators

---
+++ src/main/kotlin/IncDecOperator.kt
@@ -0,0 +1,12 @@
+fun main(){
+    //Incrementation and Decremental Operators
+
+    var numberOne = 10
+    numberOne++ // 10
+    println(++numberOne) // 12
+
+    var numberTwo = 20
+    numberTwo-- // 20
+    println(--numberTwo) // 18
+
+}
