5cfcf16981014be9858b798d3cef9af2c9eb0a6c
sheltons <shedzafyah2@gmail.com>
2023-06-28 11:13:05 +0200

    '===' for comparing memory location

--- src/main/kotlin/StringEquality.kt
+++ src/main/kotlin/StringEquality.kt
@@ -4,7 +4,8 @@ fun main (){
     var name3 = String("Shelton".toCharArray())
 //    println(".equals ${name1.equals(name2)}")
 //    println("== ${name1 == name2}")
-    println(name1 == name3)
+
+    println(name1 === name2) //using === is for comparing memory location
     println(name2.equals(name3))
 
 }
