f9e1f898f4d6141e61aec23985cad66520edb5bb
sheltons <shedzafyah2@gmail.com>
2023-06-28 13:46:10 +0200

    learning about logical operators

---
+++ src/main/kotlin/Boolean.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val isAdult = true
+    val isMale = false
+    println(isMale || isAdult) // true
+    println(!isMale || isAdult) // true
+    println(!isMale || !isAdult) // true
+    println(isMale || !isAdult) //false
+
+    //learning about logical operators
+}
