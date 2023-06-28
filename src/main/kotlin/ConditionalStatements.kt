3abcde135e123d1182c923935caa8457c61c8451
sheltons <shedzafyah2@gmail.com>
2023-06-28 16:33:41 +0200

    conditional statements

---
+++ src/main/kotlin/ConditionalStatements.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var a = 10
+    var b = 5
+
+    var result = if (a > b ) "big" else "small"
+    println(result)
+
+    /*
+     * equivalent in java
+     *
+     * var result = a > b ? "big" else "small"
+     *
+     */
+}
