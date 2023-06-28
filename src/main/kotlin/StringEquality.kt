70c1273283927483fb11b39ea73f34dba8eac814
sheltons <shedzafyah2@gmail.com>
2023-06-28 11:10:33 +0200

    how string equality works

---
+++ src/main/kotlin/StringEquality.kt
@@ -0,0 +1,10 @@
+fun main (){
+    var name1 = "Shelton"
+    var  name2 = "Shelton"
+    var name3 = String("Shelton".toCharArray())
+//    println(".equals ${name1.equals(name2)}")
+//    println("== ${name1 == name2}")
+    println(name1 == name3)
+    println(name2.equals(name3))
+
+}
