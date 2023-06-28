0cb50e3843a715567e064677b94784380a3e2c61
sheltons <shedzafyah2@gmail.com>
2023-06-28 14:00:52 +0200

    the && logical operator

--- src/main/kotlin/Boolean.kt
+++ src/main/kotlin/Boolean.kt
@@ -1,10 +1,15 @@
 fun main() {
     val isAdult = true
     val isMale = false
-    println(isMale || isAdult) // true
-    println(!isMale || isAdult) // true
-    println(!isMale || !isAdult) // true
-    println(isMale || !isAdult) //false
+    val isHappy = true
+
+//    println(isMale || isAdult) // true
+//    println(!isMale || isAdult) // true
+//    println(!isMale || !isAdult) // true
+//    println(isMale || !isAdult) //false
+
+    println(isMale && isAdult && isHappy) // false
+    println(isAdult && !isMale && isHappy) // true
 
     //learning about logical operators
 }
