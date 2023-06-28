ebf4eb03b44c8e375b0bff5d048a41c2332da1da
sheltons <shedzafyah2@gmail.com>
2023-06-28 11:27:54 +0200

    arithmetic operators

---
+++ src/main/kotlin/ArithmeticOperator.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var number1 = 10
+    var number2 =  3
+
+    println("Addition: ${number1 + number2}")
+    println("Subtraction: ${number1 - number2}")
+    println("Multiplication ${number1 * number2}")
+    println("Division ${number1 / number2}")
+    println("Modulus ${number1 % number2}")
+}
